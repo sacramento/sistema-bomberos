d5e76dfc73522fa9a897617511d5a8cc282f37f6
Nicolas Contreras <setecneuquen@gmail.com>
2025-10-07 15:08:24 +0000

    ahora siiii. Bueno ahora qu ecad a"trianfulo" o franja" tenga un efecto

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -8,9 +8,12 @@ export default function AuthLayout({
         {children}
          {/* Decorative corner element */}
         <div aria-hidden="true" className="pointer-events-none fixed bottom-0 right-0 h-full w-full">
-            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[500px] border-l-[500px] border-b-red-900/50 border-l-transparent" />
-            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[350px] border-l-[350px] border-b-red-700/50 border-l-transparent" />
-            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[200px] border-l-[200px] border-b-red-500/50 border-l-transparent" />
+            {/* Largest, darkest triangle */}
+            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[200px] border-l-[200px] border-b-red-900 border-l-transparent drop-shadow-lg" />
+            {/* Medium triangle */}
+            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[150px] border-l-[150px] border-b-red-700 border-l-transparent drop-shadow-lg" />
+            {/* Smallest, lightest triangle */}
+            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[100px] border-l-[100px] border-b-red-500 border-l-transparent drop-shadow-lg" />
         </div>
     </div>
   );
