7fe05cf11172e5de15d9fbddabada31565836216
Nicolas Contreras <setecneuquen@gmail.com>
2025-10-08 17:22:44 +0000

    sigue estando en la ventana de seleccion de modulos...

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -92,12 +92,6 @@ export default function ModuleSelectionPage() {
                 )
             })}
         </div>
-        {/* Decorative corner element */}
-         <div aria-hidden="true" className="pointer-events-none fixed bottom-0 right-0 h-full w-full">
-            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[600px] border-l-[600px] border-b-red-900 border-l-transparent" style={{filter: 'drop-shadow(8px -8px 16px rgba(0,0,0,0.1))'}} />
-            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[400px] border-l-[400px] border-b-red-700 border-l-transparent" style={{filter: 'drop-shadow(8px -8px 16px rgba(0,0,0,0.1))'}} />
-            <div className="absolute bottom-0 right-0 h-0 w-0 border-b-[200px] border-l-[200px] border-b-red-500 border-l-transparent" style={{filter: 'drop-shadow(8px -8px 16px rgba(0,0,0,0.1))'}} />
-        </div>
     </div>
   );
 }
