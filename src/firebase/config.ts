eed5ba571189e67130a7aa85f63a1edf3ffe60fb
Nicolas Contreras <setecneuquen@gmail.com>
2026-03-09 19:05:44 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "fuegoregistro",
+  "appId": "1:582746733947:web:30bdc2e9c3edb80cd2d1de",
+  "apiKey": "AIzaSyAAMEjNcyIW3n2Za5pzP8MmSbfN-YHNGKU",
+  "authDomain": "fuegoregistro.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "582746733947"
+};
